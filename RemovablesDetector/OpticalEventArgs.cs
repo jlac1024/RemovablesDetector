1b086ffe82dcd5919010c56e9b651551cf22f182
Jeff <jlac1024@gmail.com>
2018-08-03 14:21:16 -0400

    event args for optical discs

---
+++ RemovablesDetector/OpticalEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovablesDetector
+{
+    public enum OpticalEventType
+    {
+        Inserted,
+        Removed,
+    }
+
+    public class OpticalEventArgs
+    {
+        public OpticalEventType EventType { get; }
+        public OpticalDisc Disc { get; }
+    }
+}
