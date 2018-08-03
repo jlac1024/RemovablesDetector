292f7847c3f9556a2eeac86de011956172cfb4bb
Jeff <jlac1024@gmail.com>
2018-08-03 14:20:59 -0400

    model implementation for optical discs

---
+++ RemovablesDetector/OpticalDisc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovablesDetector
+{
+    public class OpticalDisc
+    {        
+        /// <summary>
+        /// Volume name of the drive (usually the description set for the drive during format)
+        /// </summary>
+        public string Volume { get; set; }
+
+        /// <summary>
+        /// Name of the drive generated for windows (usually the drive letter)
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// DirectoryInfo object for the root directory to directly interface with IO
+        /// </summary>
+        public DirectoryInfo EntryPoint { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Drive {Name}");
+            sb.AppendLine($"   Volume Label: {Volume}");
+
+            return sb.ToString();
+        }
+    }
+}
